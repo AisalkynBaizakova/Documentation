6575d2f0c29afc31a1dae223899330bebe4117e5
aisalkyn <aisalkyn.baizakova@gmail.com>
2022-01-13 00:37:27 -0500

    commit

--- src/components/AddMainTopic/AddMainTopic.js
+++ src/components/AddMainTopic/AddMainTopic.js
@@ -1,18 +1,18 @@
-import React, { useContext, useState } from 'react'
-import { documentationContext } from '../../context/DocumentationContext'
+import React, { useContext, useState } from "react";
+import { documentationContext } from "../../context/DocumentationContext";
 
 const AddMainTopic = () => {
-  const [title, setTitle] = useState('')
+  const [title, setTitle] = useState("");
 
-  const { addMainTopic } = useContext(documentationContext)
+  const { addMainTopic } = useContext(documentationContext);
 
   function handleAddTopic() {
     const newTopic = {
       topicTitle: title,
       subTopics: [],
-    }
-    addMainTopic(newTopic)
-    setTitle('')
+    };
+    addMainTopic(newTopic);
+    setTitle("");
   }
 
   return (
@@ -20,12 +20,12 @@ const AddMainTopic = () => {
       <input
         onChange={(e) => setTitle(e.target.value)}
         type="text"
-        placeholder="Название топика"
+        placeholder="Название   топика"
         value={title}
       />
       <button onClick={handleAddTopic}>Добавить</button>
     </div>
-  )
-}
+  );
+};
 
-export default AddMainTopic
+export default AddMainTopic;
